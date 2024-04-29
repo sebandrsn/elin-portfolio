3f138bb74029f60cbb4f6a51b79dc2629cebc224
Sebastian Andersson <sebastian.andersson@etonshirts.com>
2024-04-29 14:41:57 +0200

    feat: adds daisyUI theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from "next"
-import "./globals.css"
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Elin",
-  description: "A photographers portfolio",
+  title: 'Elin',
+  description: 'A photographers portfolio',
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body>{children}</body>
     </html>
   )
