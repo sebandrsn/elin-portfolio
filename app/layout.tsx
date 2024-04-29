10130bc4c029ceaa7224e45926ba609a8d8f1076
Sebastian Andersson <sebastian.andersson@etonshirts.com>
2024-04-29 15:41:35 +0200

    chore: removes daisyUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Header from './components/Header'
 
 export const metadata: Metadata = {
   title: 'Elin',
@@ -13,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="dark">
+      <Header />
       <body>{children}</body>
     </html>
   )
