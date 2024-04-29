4261a53ee8c1cf0b390b575c7e9d69c640a191ea
Sebastian Andersson <sebastian.andersson@etonshirts.com>
2024-04-29 12:02:57 +0200

    feat: npx create

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Elin",
+  description: "A photographers portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
