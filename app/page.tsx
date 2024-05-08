f26e75df11d08b5bc82911e15076742407ea5461
Sebastian Andersson <sebastian.andersson@etonshirts.com>
2024-05-08 06:06:57 +0200

    fix: removes image from startpage

--- app/page.tsx
+++ app/page.tsx
@@ -3,70 +3,58 @@ import mountains from '@/public/images/jordan-steranka-S2uQdHTXbbc-unsplash.jpg'
 
 export default function Home() {
   return (
-    <>
-      <div className="absolute w-screen h-screen">
-        <Image
-          alt="Mountains"
-          src={mountains}
-          placeholder="blur"
-          fill
-          sizes="100vh"
-        ></Image>
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna
-        nec justo finibus consectetur. Nam non est mauris. Vestibulum euismod,
-        lorem eget consequat molestie, elit erat tempus nibh, et aliquet ex
-        lorem quis dui. Nam a ullamcorper dui. Aliquam vulputate faucibus magna
-        a rutrum. Mauris et pulvinar ligula. Fusce aliquet aliquam placerat.
-        Donec sed elit risus. Nulla est mi, iaculis eget massa non, interdum
-        mollis nibh. Ut non erat dapibus ex tincidunt lobortis tempus non odio.
-        Pellentesque gravida tortor et est malesuada, ut convallis felis
-        sollicitudin. Aliquam erat volutpat. Curabitur vitae sem vitae turpis
-        scelerisque laoreet non vitae massa. Nulla sed magna sit amet lacus
-        laoreet efficitur. Mauris pharetra pellentesque augue id convallis. Sed
-        dignissim efficitur nisi, ut consequat nunc scelerisque vel. Quisque at
-        nibh a purus mattis viverra. Nulla facilisi. Nunc fringilla nulla sit
-        amet velit faucibus commodo. Fusce tristique augue mollis mi tincidunt,
-        vel lobortis lectus malesuada. Fusce tincidunt est et turpis congue
-        euismod. Praesent sed ligula vel dui dictum pharetra ut ut lectus. Nunc
-        porttitor a purus in feugiat. Suspendisse non arcu in mi molestie
-        luctus. Etiam dapibus consectetur ultrices. Phasellus vehicula augue id
-        ante rutrum, id laoreet est iaculis. Vivamus quis nisi quis eros
-        consequat ultrices ac sed mauris. Nam viverra est vel molestie
-        vulputate. Praesent vel nibh sed sapien tincidunt suscipit. Ut ultrices,
-        leo eu maximus bibendum, magna lectus aliquam tellus, nec lobortis
-        tortor sapien a tellus. Nullam scelerisque nec leo at ullamcorper. In
-        hac habitasse platea dictumst. Nunc auctor mi purus, eu volutpat dolor
-        convallis eget. Nunc dictum ac est vitae facilisis. In id purus
-        vehicula, tempor urna vitae, interdum tortor. Cras lectus lectus,
-        facilisis eu tempor eu, aliquet sit amet ipsum. Curabitur quis sem non
-        orci pretium vulputate. In eu tristique nulla. Vestibulum tempor laoreet
-        sapien, quis pretium ex facilisis viverra. Aliquam augue dolor,
-        imperdiet ut gravida nec, fermentum vel metus. Suspendisse eu auctor
-        est. Sed lacinia dui a sem fringilla rhoncus in luctus dolor. Fusce
-        purus odio, commodo in orci quis, commodo ultrices mauris. In
-        sollicitudin eleifend elit convallis finibus. Nam lacus quam,
-        sollicitudin ut posuere ac, aliquet in ante. Donec condimentum maximus
-        nisl in commodo. Maecenas sem ligula, consequat sed lectus a, pharetra
-        eleifend sem. Suspendisse hendrerit eros vitae suscipit pellentesque.
-        Fusce sed pellentesque nulla, non mollis metus. Morbi fringilla diam non
-        sapien tincidunt, vitae tincidunt enim vestibulum. Nam interdum arcu
-        eget finibus rutrum. Fusce vulputate diam eget blandit semper. Nulla
-        facilisi. Morbi euismod erat sit amet convallis posuere. Quisque
-        fermentum sollicitudin lacus, sed sagittis ipsum mollis quis. Interdum
-        et malesuada fames ac ante ipsum primis in faucibus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus. Nunc commodo eleifend
-        ante vitae euismod. Pellentesque ac eros sapien. Cras ligula diam,
-        eleifend at venenatis vitae, euismod nec nibh. Sed vel erat quis tortor
-        sollicitudin venenatis. Mauris sollicitudin dictum condimentum. Aenean
-        mattis aliquet neque, nec congue tellus. Sed ac arcu nec libero tempus
-        rutrum. Nunc laoreet nibh vitae nunc rhoncus posuere. Nullam eu
-        malesuada sem. Sed vel arcu sem. Nam congue dignissim nibh eu venenatis.
-        Nullam malesuada non nibh nec rhoncus. Cras accumsan dolor lorem, at
-        vulputate arcu bibendum a. Etiam scelerisque aliquet risus vitae
-        sodales. Maecenas consectetur mauris sit amet ipsum sagittis porta.
-      </div>
-    </>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nec magna
+      nec justo finibus consectetur. Nam non est mauris. Vestibulum euismod,
+      lorem eget consequat molestie, elit erat tempus nibh, et aliquet ex lorem
+      quis dui. Nam a ullamcorper dui. Aliquam vulputate faucibus magna a
+      rutrum. Mauris et pulvinar ligula. Fusce aliquet aliquam placerat. Donec
+      sed elit risus. Nulla est mi, iaculis eget massa non, interdum mollis
+      nibh. Ut non erat dapibus ex tincidunt lobortis tempus non odio.
+      Pellentesque gravida tortor et est malesuada, ut convallis felis
+      sollicitudin. Aliquam erat volutpat. Curabitur vitae sem vitae turpis
+      scelerisque laoreet non vitae massa. Nulla sed magna sit amet lacus
+      laoreet efficitur. Mauris pharetra pellentesque augue id convallis. Sed
+      dignissim efficitur nisi, ut consequat nunc scelerisque vel. Quisque at
+      nibh a purus mattis viverra. Nulla facilisi. Nunc fringilla nulla sit amet
+      velit faucibus commodo. Fusce tristique augue mollis mi tincidunt, vel
+      lobortis lectus malesuada. Fusce tincidunt est et turpis congue euismod.
+      Praesent sed ligula vel dui dictum pharetra ut ut lectus. Nunc porttitor a
+      purus in feugiat. Suspendisse non arcu in mi molestie luctus. Etiam
+      dapibus consectetur ultrices. Phasellus vehicula augue id ante rutrum, id
+      laoreet est iaculis. Vivamus quis nisi quis eros consequat ultrices ac sed
+      mauris. Nam viverra est vel molestie vulputate. Praesent vel nibh sed
+      sapien tincidunt suscipit. Ut ultrices, leo eu maximus bibendum, magna
+      lectus aliquam tellus, nec lobortis tortor sapien a tellus. Nullam
+      scelerisque nec leo at ullamcorper. In hac habitasse platea dictumst. Nunc
+      auctor mi purus, eu volutpat dolor convallis eget. Nunc dictum ac est
+      vitae facilisis. In id purus vehicula, tempor urna vitae, interdum tortor.
+      Cras lectus lectus, facilisis eu tempor eu, aliquet sit amet ipsum.
+      Curabitur quis sem non orci pretium vulputate. In eu tristique nulla.
+      Vestibulum tempor laoreet sapien, quis pretium ex facilisis viverra.
+      Aliquam augue dolor, imperdiet ut gravida nec, fermentum vel metus.
+      Suspendisse eu auctor est. Sed lacinia dui a sem fringilla rhoncus in
+      luctus dolor. Fusce purus odio, commodo in orci quis, commodo ultrices
+      mauris. In sollicitudin eleifend elit convallis finibus. Nam lacus quam,
+      sollicitudin ut posuere ac, aliquet in ante. Donec condimentum maximus
+      nisl in commodo. Maecenas sem ligula, consequat sed lectus a, pharetra
+      eleifend sem. Suspendisse hendrerit eros vitae suscipit pellentesque.
+      Fusce sed pellentesque nulla, non mollis metus. Morbi fringilla diam non
+      sapien tincidunt, vitae tincidunt enim vestibulum. Nam interdum arcu eget
+      finibus rutrum. Fusce vulputate diam eget blandit semper. Nulla facilisi.
+      Morbi euismod erat sit amet convallis posuere. Quisque fermentum
+      sollicitudin lacus, sed sagittis ipsum mollis quis. Interdum et malesuada
+      fames ac ante ipsum primis in faucibus. Interdum et malesuada fames ac
+      ante ipsum primis in faucibus. Nunc commodo eleifend ante vitae euismod.
+      Pellentesque ac eros sapien. Cras ligula diam, eleifend at venenatis
+      vitae, euismod nec nibh. Sed vel erat quis tortor sollicitudin venenatis.
+      Mauris sollicitudin dictum condimentum. Aenean mattis aliquet neque, nec
+      congue tellus. Sed ac arcu nec libero tempus rutrum. Nunc laoreet nibh
+      vitae nunc rhoncus posuere. Nullam eu malesuada sem. Sed vel arcu sem. Nam
+      congue dignissim nibh eu venenatis. Nullam malesuada non nibh nec rhoncus.
+      Cras accumsan dolor lorem, at vulputate arcu bibendum a. Etiam scelerisque
+      aliquet risus vitae sodales. Maecenas consectetur mauris sit amet ipsum
+      sagittis porta.
+    </div>
   )
 }
