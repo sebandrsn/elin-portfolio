7907841080a08f41bb0a4e4b5bf2dc9e20d4d5a9
Sebastian Andersson <sebastiandersson@gmail.com>
2025-04-19 09:27:53 +0200

    feat: use Tailwindcss instead of isMobile logic

--- components/ResponsiveBackgroundImage.tsx
+++ components/ResponsiveBackgroundImage.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 interface ResponsiveBackgroundImageProps {
   mobileImage: string;
   desktopImage: string;
@@ -12,26 +10,29 @@ export default function ResponsiveBackgroundImage({
   mobileImage,
   desktopImage,
   className,
-}: ResponsiveBackgroundImageProps) {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth <= 640);
-    checkMobile();
-    window.addEventListener("resize", checkMobile);
-    return () => window.removeEventListener("resize", checkMobile);
-  }, []);
-
+}: Readonly<ResponsiveBackgroundImageProps>) {
   return (
-    <div
-      className={className}
-      style={{
-        backgroundImage: `url(${isMobile ? mobileImage : desktopImage})`,
-        backgroundAttachment: "fixed",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    />
+    <>
+      <div
+        className={`sm:hidden ${className}`}
+        style={{
+          backgroundImage: `url(${mobileImage})`,
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      />
+      <div
+        className={`hidden sm:block ${className}`}
+        style={{
+          backgroundImage: `url(${desktopImage})`,
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      />
+    </>
   );
 }
