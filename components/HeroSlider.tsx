c93135136df0cb716b86b37ba14becfd68224761
Sebastian Andersson <sebastiandersson@gmail.com>
2025-06-01 20:07:10 +0200

    feat: adds HeroSlider

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -1,3 +1,54 @@
+"use client";
+
+import { useEffect } from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import Image from "next/image";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+
+import { EffectFade, Pagination, Navigation } from "swiper/modules";
+
+function AutoAdvance() {
+  const swiper = useSwiper();
+  useEffect(() => {
+    const timer = setInterval(() => {
+      swiper.slideNext();
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [swiper]);
+  return null;
+}
+
 export default function HeroSlider({ blok }: any) {
-  return <div>this is a slider</div>;
+  return (
+    <div className="relative h-screen w-full">
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        loop={true}
+        effect={"fade"}
+        modules={[EffectFade, Pagination, Navigation]}
+        className="h-full w-full"
+      >
+        <AutoAdvance />
+        {blok.heroes.map((hero: any) => (
+          <SwiperSlide key={hero._uid} className="relative h-full w-full">
+            <Image
+              src={hero.image.filename}
+              alt={hero.image.alt}
+              fill
+              className="object-cover"
+              loading="eager"
+              priority
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
