7e144df5240c19cfcd4841837cbb100ea4f7615e
Sebastian Andersson <sebastiandersson@gmail.com>
2025-09-04 15:47:45 +0200

    feat: remove skeleton since suspense only works for client side components

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,5 @@
 import { ISbStoriesParams, StoryblokClient } from "@storyblok/react";
 import { StoryblokStory } from "@storyblok/react/rsc";
-import { Suspense } from "react";
-import GallerySkeleton from "@/components/gallery/GallerySkeleton";
 import { getStoryblokApi } from "@/service/storyblok";
 
 export const revalidate = 86400; // Revalidate every 24 hours
@@ -9,11 +7,7 @@ export const revalidate = 86400; // Revalidate every 24 hours
 export default async function Gallery() {
   const story = await fetchGalleryPage();
 
-  return (
-    <Suspense fallback={<GallerySkeleton />}>
-      <StoryblokStory story={story} />
-    </Suspense>
-  );
+  return <StoryblokStory story={story} />;
 }
 
 async function fetchGalleryPage() {
--- app/gallery/paintings/page.tsx
+++ app/gallery/paintings/page.tsx
@@ -1,7 +1,5 @@
 import { ISbStoriesParams, StoryblokClient } from "@storyblok/react";
 import { StoryblokStory } from "@storyblok/react/rsc";
-import { Suspense } from "react";
-import GallerySkeleton from "@/components/gallery/GallerySkeleton";
 import { getStoryblokApi } from "@/service/storyblok";
 
 export const revalidate = 86400; // Revalidate every 24 hours
@@ -9,11 +7,7 @@ export const revalidate = 86400; // Revalidate every 24 hours
 export default async function PaintingsGallery() {
   const story = await fetchPaintingsGalleryPage();
 
-  return (
-    <Suspense fallback={<GallerySkeleton />}>
-      <StoryblokStory story={story} />
-    </Suspense>
-  );
+  return <StoryblokStory story={story} />;
 }
 
 async function fetchPaintingsGalleryPage() {
