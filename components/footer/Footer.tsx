d3881fcf75953645a7e94d141d04908d4e8ab9e8
Sebastian Andersson <sebastiandersson@gmail.com>
2025-09-06 16:14:38 +0200

    feat: removes framer motion for footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { motion } from "framer-motion";
-
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   const footerText = `© ${currentYear} Elin Åsedahl. Alla rättigheter förbehållna.`;
@@ -9,17 +5,7 @@ export default function Footer() {
     <div className="h-auto bg-black text-white lg:h-48">
       <div className="mx-auto flex h-full w-1/2 flex-col justify-between">
         <div className="my-9 flex flex-col gap-11 lg:mt-8 lg:flex-row">
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.3 }}
-            variants={{
-              visible: { opacity: 1, y: 0 },
-              hidden: { opacity: 0, y: 30 },
-            }}
-            className="flex flex-col items-center justify-center text-4xl lg:w-1/2"
-          >
+          <div className="flex flex-col items-center justify-center text-4xl lg:w-1/2">
             Contact
             <a
               href="mailto:elin.asedahl@hotmail.com"
@@ -27,18 +13,8 @@ export default function Footer() {
             >
               elin.asedahl@hotmail.com
             </a>
-          </motion.div>
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.3 }}
-            variants={{
-              visible: { opacity: 1, y: 0 },
-              hidden: { opacity: 0, y: 30 },
-            }}
-            className="flex flex-col items-center justify-center text-4xl lg:w-1/2"
-          >
+          </div>
+          <div className="flex flex-col items-center justify-center text-4xl lg:w-1/2">
             Social
             <a
               href="https://instagram.com/elinsfotogalleri"
@@ -48,7 +24,7 @@ export default function Footer() {
             >
               Instagram
             </a>
-          </motion.div>
+          </div>
         </div>
         <div className="hidden justify-center lg:flex lg:pb-4">
           {footerText}
