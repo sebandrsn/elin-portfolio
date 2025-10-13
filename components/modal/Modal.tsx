8f720c62ec9440df3fdd34e0bdf583f158e34de6
Sebastian Andersson <sebastiandersson@gmail.com>
2025-10-13 20:13:25 +0200

    feat: adds event listener to close modal

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { GalleryItem } from "@/.storyblok/types/337287/storyblok-components";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useRef } from "react";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -18,6 +19,25 @@ export default function Modal({
   galleryItems: GalleryItem[];
   handleCloseModal: () => void;
 }>) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleMouseDown = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        handleCloseModal();
+      }
+    };
+
+    document.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, []);
+
   if (!galleryItems || galleryItems.length === 0) return null;
 
   const initialSlide = galleryItems.findIndex(
@@ -26,41 +46,43 @@ export default function Modal({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs">
-      <Swiper
-        className="h-[80vh] w-[min(1100px,90vw)] rounded-2xl bg-white p-4 shadow-lg"
-        loop
-        effect="fade"
-        fadeEffect={{ crossFade: true }}
-        navigation
-        modules={[EffectFade, Pagination, Navigation]}
-        initialSlide={initialSlide >= 0 ? initialSlide : 0}
-      >
-        {galleryItems
-          .filter((item: GalleryItem) => item.image?.filename)
-          .map((item: GalleryItem) => (
-            <SwiperSlide key={item._uid}>
-              <div className="relative h-full w-full">
-                <Image
-                  src={item.image?.filename!}
-                  alt={item.image?.alt ?? "Gallery image"}
-                  fill
-                  className="object-contain"
-                  loading="eager"
-                  priority
-                  sizes="(max-width: 1023px) 100vw, 2100px"
-                />
-                <div className="absolute inset-0 flex items-start justify-end">
-                  <button
-                    className="cursor-pointer rounded p-2"
-                    onClick={handleCloseModal}
-                  >
-                    Close
-                  </button>
+      <div ref={modalRef}>
+        <Swiper
+          className="h-[80vh] w-[min(1100px,90vw)] rounded-2xl bg-white p-4 shadow-lg"
+          loop
+          effect="fade"
+          fadeEffect={{ crossFade: true }}
+          navigation
+          modules={[EffectFade, Pagination, Navigation]}
+          initialSlide={initialSlide >= 0 ? initialSlide : 0}
+        >
+          {galleryItems
+            .filter((item: GalleryItem) => item.image?.filename)
+            .map((item: GalleryItem) => (
+              <SwiperSlide key={item._uid}>
+                <div className="relative h-full w-full">
+                  <Image
+                    src={item.image?.filename!}
+                    alt={item.image?.alt ?? "Gallery image"}
+                    fill
+                    className="object-contain"
+                    loading="eager"
+                    priority
+                    sizes="(max-width: 1023px) 100vw, 2100px"
+                  />
+                  <div className="absolute inset-0 flex items-start justify-end">
+                    <button
+                      className="cursor-pointer rounded p-2"
+                      onClick={handleCloseModal}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
-      </Swiper>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
